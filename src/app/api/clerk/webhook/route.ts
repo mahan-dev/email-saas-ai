b6f70c1e869782f831a8b56b19745d75b77777d7
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-03 23:10:21 +0330

    api/clerk 2

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -4,18 +4,15 @@ export async function POST(req: Request) {
   const { data } = await req.json();
 
   const emailAddress = data.email_addresses?.[0]?.email_address;
-  const firstName = data.first_name;
-  const lastName = data.last_name;
-  const imageUrl = data.image_url;
-  const id = data.id;
+
 
   await db.user.create({
     data: {
-      id,
+      id: data.id,
       emailAddress,
-      firstName,
-      lastName,
-      imageUrl,
+      firstName: data.first_name ?? "",
+      lastName: data.last_name ?? "",
+      imageUrl: data.image_url ?? "",
     },
   });
 
