6b45fe268886531a4fce9063eca6bfdece7ba39c
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-03 13:06:23 +0330

    prisma schema

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { SignIn } from "@clerk/nextjs";
 
 const page = () => {
-  return <SignIn />;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignIn />
+    </div>
+  );
 };
 
 export default page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  console.log("CLERK PROXY RUNNING:", req.nextUrl.pathname);
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
