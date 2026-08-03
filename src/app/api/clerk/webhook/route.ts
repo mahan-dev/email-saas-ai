a19ac208e43919911f698876eed1f4964a7d79f5
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-03 22:46:37 +0330

    api/clerk changes

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,25 +1,14 @@
 import { db } from "@/server/db";
-
-export const POST = async (req: Request) => {
+export async function POST(req: Request) {
   const { data } = await req.json();
-
-  const emailAddress = data.email_addresses[0].email_address;
+  const emailAddress = data.email_addresses?.[0]?.email_address;
   const firstName = data.first_name;
   const lastName = data.last_name;
   const imageUrl = data.image_url;
   const id = data.id;
-
   await db.user.create({
-    data: {
-      id,
-      emailAddress,
-      firstName,
-      lastName,
-      imageUrl,
-    },
+    data: { id, emailAddress, firstName, lastName, imageUrl },
   });
-
   console.log("data created");
-
-  return new Response("web received", data);
-};
+  return new Response("web received", { status: 200 });
+}
