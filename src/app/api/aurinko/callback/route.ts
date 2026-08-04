507846a2494bd689a7c8250543093a2504b053b8
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-04 20:14:01 +0330

    api/aurinko changes-3

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,8 +1,9 @@
-import { getAuth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: Response) => {
-  const { userId } = await getAuth;
+  const { userId } = await auth;
+  console.log("🛫 ~ route.ts:4 ~ req:", req);
   console.log("🛫 ~ route.ts:4 ~ userId:", userId);
   return NextResponse.json({ status: "Success", message: "hello world" });
 };
