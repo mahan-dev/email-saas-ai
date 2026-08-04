58dd192c86869d3cb3113fad230bebf9f07a7f36
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-04 20:21:10 +0330

    api/aurinko changes-4

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -5,5 +5,8 @@ export const GET = async (req: Response) => {
   const { userId } = await auth;
   console.log("🛫 ~ route.ts:4 ~ req:", req);
   console.log("🛫 ~ route.ts:4 ~ userId:", userId);
-  return NextResponse.json({ status: "Success", message: "hello world" });
+  return NextResponse.json(
+    { status: "Success", message: "hello world" },
+    { status: 200 },
+  );
 };
