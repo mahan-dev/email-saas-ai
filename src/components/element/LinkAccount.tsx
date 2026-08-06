17898d5834554baf68fb08bb6e4ec6d074972592
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-06 22:49:55 +0330

    LinkAccount changes

--- src/components/element/LinkAccount.tsx
+++ src/components/element/LinkAccount.tsx
@@ -3,12 +3,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { getAurinkoAuthUrl } from "@/lib/aurniko";
+import { url } from "inspector";
 
 const LinkAccount = () => {
   return (
     <Button
       onClick={async () => {
         const res = await getAurinkoAuthUrl("Google");
+        window.location.href = url;
         console.log("🛤️ ~ page.tsx:9 ~ res:", res);
       }}
     >
